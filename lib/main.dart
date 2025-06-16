1c92660c0c43befa61ff5e8911e610454705828e
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-06-16 15:57:50 +0300

    Locked in portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:noir_journal/screens/home.dart';
 import 'package:noir_journal/screens/intro.dart';
 import 'package:noir_journal/screens/settings.dart';
 import 'package:noir_journal/theme/app_theme.dart';
+import 'utils/orientation_lock.dart';
 
 ValueNotifier<ThemeMode>? globalThemeModeNotifier;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await lockPortraitMode();
   final prefs = await SharedPreferences.getInstance();
   final isDark = prefs.getBool('isDarkTheme') ?? false;
   globalThemeModeNotifier = ValueNotifier(
