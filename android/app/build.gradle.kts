ed27bce3703e236feb6b652caa10f0dd6482506e
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-06-17 15:59:40 +0300

    Fixed keystore and proguard

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,7 +2,7 @@ import java.util.Properties
 import java.io.FileInputStream
 
 val keystoreProperties = Properties().apply {
-    val file = rootProject.file("../key.properties")
+    val file = rootProject.file("key.properties")
     if (file.exists()) {
         load(FileInputStream(file))
     }
@@ -11,7 +11,6 @@ val keystoreProperties = Properties().apply {
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -19,6 +18,14 @@ android {
     namespace = "com.AS.noir_journal"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+
+    defaultConfig {
+        applicationId = "com.AS.noir_journal"
+        minSdk = 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -30,28 +37,26 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.AS.noir_journal"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+    signingConfigs {
+        create("release") {
+            val storeFilePath = keystoreProperties["storeFile"] as String?
+            if (!storeFilePath.isNullOrBlank()) {
+                storeFile = file(storeFilePath)
+                storePassword = keystoreProperties["storePassword"] as String?
+                keyAlias = keystoreProperties["keyAlias"] as String?
+                keyPassword = keystoreProperties["keyPassword"] as String?
+            }
+        }
     }
 
     buildTypes {
         release {
-            val storeFilePath = keystoreProperties["storeFile"] as String?
-            if (!storeFilePath.isNullOrBlank()) {
-                signingConfig = signingConfigs.create("release").apply {
-                    storeFile = file(storeFilePath)
-                    storePassword = keystoreProperties["storePassword"] as String?
-                    keyAlias = keystoreProperties["keyAlias"] as String?
-                    keyPassword = keystoreProperties["keyPassword"] as String?
-                }
-            }
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,2 @@
+# Add ProGuard rules here if needed. By default, Flutter apps do not require custom rules.
+# For most Flutter apps, this file can remain empty unless you use plugins that require specific rules.
